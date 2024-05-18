9e9710cd92c789ec7a58031b15efb9a16e5c3031
Your Name <you@example.com>
2024-05-18 07:30:13 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -153,7 +153,8 @@ Node* DFS(Node* initial, int* cont)
    push(S, initial);
    while(!is_empty(S))
    {
-      Node* ini = pop(S);
+      Node* ini = top(S);
+      pop(S);
       if(is_final(ini)) return ini;
       List * adj = get_adj_nodes(ini);
       while(!is_empty(adj))
@@ -162,9 +163,8 @@ Node* DFS(Node* initial, int* cont)
          push(S, adjunt);
          next(adj);
       }   
-      
+      free(ini);
    }
-   free(ini);
   return NULL;
 }
 
